fe370231c3a7a2aacd382fe2ebbdfc30b2880f40
bimal <bimal@OKICOM>
2023-05-10 16:06:49 +0900

    Test Api Created

---
+++ Data/TodoDatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApi.Model;
+
+namespace TodoListApi.Data
+{
+    public class TodoDatabaseContext : DbContext
+    {
+        public TodoDatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
